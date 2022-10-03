74672cc4735febdba08b10873702c258975e23b9
kartikeysharmaks <kartikeysharma1616@gmail.com>
2022-10-03 17:03:43 +0530

    corrections

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -1,44 +1,15 @@
+import React from "react";
 import "./EventPage.css";
 import { HiOutlineCalendar, HiOutlineLocationMarker } from "react-icons/hi";
-import React, { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
-import { db } from "../firebase";
-
-
-const PropertiesList = () => {
-  const [properties, setProperties] = useState([]);
-  const [loading, setLoading] = useState(false);
 
 const EventPage = () => {
 
-  useEffect(() => {
-    setLoading(true);
-    const getProperties = onSnapshot(
-      collection(db, "properties"),
-      (snapshot) => {
-        let list = [];
-        snapshot.docs.forEach((doc) => {
-          list.push({ id: doc.id, ...doc.data() });
-        });
-        setProperties(list);
-        setLoading(false);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-
-    return () => {
-      getProperties();
-    };
-  }, []);
+  
   return (
     <div className="Event-Section">
       <div className="left">
         <div className="event-text">
-          {properties.map((event,index)=>(
-            <>
-            <div className="event-heading">Birthday Bash</div>
+          <div className="event-heading">Birthday Bash</div>
           <div className="event-host-name">hosted by Elysia</div>
           <div className="event-date">
             <div className="event-date-icon">
@@ -58,14 +29,8 @@ const EventPage = () => {
               <div className="address-code">249980 Paris</div>
             </div>
           </div>
-        
-
-            </>
-
-          ))}
-          
+        </div>
         <div className="right" />
-      </div>
       </div>
     </div>
   );
